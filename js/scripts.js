712c9c24e0d1f36000b9e1851eb4da11858b8408
Peter Kingori <kpndegwa@gmail.com>
2019-07-22 12:38:23 +0300

    Work on the hover effect on the images in the Portfolio section to fit the requirements. The earlier work done was not right.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function () {
+$(function () {
 	jQuery('div#design').click(function () {
 		jQuery('#design-text').toggle();
 		jQuery('#design-img').toggle();
@@ -14,29 +14,37 @@ jQuery(document).ready(function () {
 
 	jQuery('div.project1').hover(function () {
 		jQuery('div.projectName1').toggle();
+		jQuery('img.projectImage1').toggleClass('opaque1');
 	});
 	jQuery('div.project2').hover(function () {
 		jQuery('div.projectName2').toggle();
+		jQuery('img.projectImage2').toggleClass('opaque1');		
 	});
 	jQuery('div.project3').hover(function () {
 		jQuery('div.projectName3').toggle();
+		jQuery('img.projectImage3').toggleClass('opaque1');		
 	});
 	jQuery('div.project4').hover(function () {
 		jQuery('div.projectName4').toggle();
+		jQuery('img.projectImage4').toggleClass('opaque1');		
 	});
 	jQuery('div.project5').hover(function () {
 		jQuery('div.projectName5').toggle();
+		jQuery('img.projectImage5').toggleClass('opaque1');		
 	});
 	jQuery('div.project6').hover(function () {
 		jQuery('div.projectName6').toggle();
+		jQuery('img.projectImage6').toggleClass('opaque1');		
 	});
 	jQuery('div.project7').hover(function () {
 		jQuery('div.projectName7').toggle();
+		jQuery('img.projectImage7').toggleClass('opaque1');		
 	});
 	jQuery('div.project8').hover(function () {
 		jQuery('div.projectName8').toggle();
+		jQuery('img.projectImage8').toggleClass('opaque1');		
 	});
-
+	
 	jQuery('form#contact-form').submit(function (event) {
 		event.preventDefault();
 		var userName = document.getElementById('name').value;
